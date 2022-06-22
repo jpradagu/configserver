76a1bb5942debf63550c9086c371a82c004f0965
Jimmy Prada <jipragu@emeal.nttdata.com>
2022-06-22 15:39:44 -0500

    fix checkstyle.xml

--- src/test/java/com/nttdata/bootcamp/config/ConfigserverApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/config/ConfigserverApplicationTests.java
@@ -3,11 +3,14 @@ package com.nttdata.bootcamp.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * ConfigServerApplicationTest.
+ */
 @SpringBootTest
 class ConfigserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
